82f167172d6c011c89f74daf15cda06afb87daa1
derrop <roclaromi@gmail.com>
2019-09-22 15:03:21 +0200

    Implemented clan detection

--- src/main/java/de/derrop/labymod/addons/cores/CoresAddon.java
+++ src/main/java/de/derrop/labymod/addons/cores/CoresAddon.java
@@ -3,6 +3,7 @@ package de.derrop.labymod.addons.cores;
  * Created by derrop on 22.09.2019
  */
 
+import de.derrop.labymod.addons.cores.clan.ClanDetector;
 import de.derrop.labymod.addons.cores.listener.PlayerLoginLogoutListener;
 import de.derrop.labymod.addons.cores.listener.PlayerStatsListener;
 import de.derrop.labymod.addons.cores.party.PartyDetector;
@@ -21,6 +22,7 @@ public class CoresAddon extends LabyModAddon {
 
     private StatsParser statsParser = new StatsParser();
     private PartyDetector partyDetector = new PartyDetector();
+    private ClanDetector clanDetector = new ClanDetector();
     private ExecutorService executorService = Executors.newCachedThreadPool();
 
     public StatsParser getStatsParser() {
@@ -37,17 +39,40 @@ public class CoresAddon extends LabyModAddon {
         this.getApi().getEventManager().register(new PlayerStatsListener(this));
         this.getApi().getEventManager().register(new PlayerLoginLogoutListener(this));
         this.getApi().getEventManager().register(this.partyDetector);
-        this.getApi().getEventManager().registerOnQuit(serverData -> this.partyDetector.handleLeaveParty());
+        this.getApi().getEventManager().registerOnJoin(serverData -> {
+            //todo check for all online players stats and warn
+        });
+        this.getApi().getEventManager().registerOnQuit(serverData -> {
+            this.partyDetector.handleLeaveParty();
+            this.clanDetector.clearCache();
+        });
         System.out.println("[CoresStats] Successfully enabled the addon!");
     }
 
     public void warnOnGoodStats(PlayerStatistics statistics) {
+        if (LabyModCore.getMinecraft().getPlayer().getName().equals(statistics.getName())) { //not warning for my good stats
+            return;
+        }
+
+        if (this.partyDetector.getCurrentPartyMembers().contains(statistics.getName())) { //don't warn when the player is in my party todo not tested if this works
+            System.out.println("Party contains " + statistics.getName() + ", not warning the player!");
+            return;
+        }
+        String selfClan = this.clanDetector.getSelfClanShortcut();
+        String otherClan = this.clanDetector.getClanShortcut(statistics.getName());
+        if (selfClan != null && selfClan.equals(otherClan)) { //don't warn when the player is in my clan todo not tested if this works
+            System.out.println("Clan contains " + statistics.getName() + ", not warning the player!");
+            return;
+        }
+
         Map<String, String> stats = statistics.getStats();
         int kills = 0;
         int deaths = 0;
         double kd = 0;
         int rank = 0;
         double winRate = 0;
+        int playedGames = 0;
+        int wonGames = 0;
         if (stats.containsKey("kills")) {
             kills = Integer.parseInt(stats.get("kills"));
         }
@@ -63,11 +88,18 @@ public class CoresAddon extends LabyModAddon {
         if (stats.containsKey("winRate")) {
             winRate = Double.parseDouble(stats.get("winRate"));
         }
+        if (stats.containsKey("playedGames")) {
+            playedGames = Integer.parseInt(stats.get("playedGames"));
+        }
+        if (stats.containsKey("wonGames")) {
+            wonGames = Integer.parseInt(stats.get("wonGames"));
+        }
         if (rank > 0 && rank <= 300) {
             LabyModCore.getMinecraft().displayMessageInChat("§4WARNUNG: §7Spieler §e" + statistics.getName() + " §7ist Platz §e" + rank);
         }
-        if (winRate >= 75) {
-            LabyModCore.getMinecraft().displayMessageInChat("§4WARNUNG: §7Spieler §e" + statistics.getName() + " §7hat eine Siegwahrscheinlichkeit von über §e" + winRate + " %");
+        if (winRate >= 75 && playedGames >= 30) {
+            LabyModCore.getMinecraft().displayMessageInChat("§4WARNUNG: §7Spieler §e" + statistics.getName() + " §7hat eine Siegwahrscheinlichkeit von §e" + winRate + " %" +
+                    " §8(Gespielt: §e" + playedGames + "§8; Gewonnen: §e" + wonGames + "§8)");
         }
     }
 
